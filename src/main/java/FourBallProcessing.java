3a06d45b9616413aa6972ddec67d10ed56764e22
Steffi <steffiarockia.22cs@licet.ac.in>
2022-03-26 21:42:40 +0530

    Modify FourBallProcessing.java

--- src/main/java/FourBallProcessing.java
+++ src/main/java/FourBallProcessing.java
@@ -1,11 +1,22 @@
 import processing.core.PApplet;
 
+import java.awt.*;
+
 public class FourBallProcessing extends PApplet {
 
-    public static final int Height = 480;
-    public static final int Width = 640;
+    public static final int HEIGHT = 480;
+    public static final int WIDTH = 640;
     public static final int Diameter = 10;
     public static int position = 0;
+    public static final int SPEED_1 = 1;
+    public static final int SPEED_2 = 2;
+    public static final int SPEED_3 = 3;
+    public static final int SPEED_4 = 4;
+    int xCoordinate1 = 0;
+    int xCoordinate2 = 0;
+    int xCoordinate3 = 0;
+    int xCoordinate4 = 0;
+
 
     public static void main(String[] args) {
         PApplet.main("FourBallProcessing",args);
@@ -13,20 +24,18 @@ public class FourBallProcessing extends PApplet {
 
     @Override
     public void settings() {
-        super.settings();
-        size(Width, Height);
+        size(WIDTH, HEIGHT);
+    }
+    @Override
+    public void setup() {
     }
 
     @Override
     public void draw() {
-        for (int number = 1; number<= 4; number++) {
-            ball(number*position,number*Height/5);
-        }
-        position +=1;
-    }
-
-    private void ball(int positionX, int positionY) {
-        ellipse(positionX,positionY,Diameter, Diameter);
+        ellipse(xCoordinate1 += SPEED_1, HEIGHT*1/5, Diameter, Diameter);
+        ellipse(xCoordinate2 += SPEED_2, HEIGHT*2/5, Diameter, Diameter);
+        ellipse(xCoordinate3 += SPEED_3, HEIGHT*3/5, Diameter, Diameter);
+        ellipse(xCoordinate4 += SPEED_4, HEIGHT*4/5, Diameter, Diameter);
     }
 
 }
